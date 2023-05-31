2994bbc8e731a0f51dcb5496e30104e6df8de9e5
R <divya.radhakrishnan@thermofisher.com>
2023-05-31 13:10:37 +0530

    Adding the application for azure database updation

---
+++ azure-poc/src/test/java/com/example/azurepoc/AzurePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
